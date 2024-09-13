606e3edfb7fe34521d6ada2db0b4a312ac0e4c89
Anshee-singh <ansheesingh06@gmail.com>
2024-09-13 22:43:42 +0530

    Files

--- index.js
+++ index.js
@@ -1,18 +1,63 @@
-let cartCount = 0;
-const cartButton = document.querySelector('.cart button');
-const addToCartButtons = document.querySelectorAll('.add-to-cart');
+let foods = document.getElementById("foods");
+let orders = document.getElementById("orders");
+let dishs = document.getElementById("dishs");
+let trackBtn = document.getElementById("trackBtn");
 
-// Add event listener to each "Add to Cart" button
-addToCartButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const price = button.getAttribute('data-price');
-        cartCount++;
-        updateCart();
-        alert(`Item added to cart! Price: ${price}`);
-    });
-});
 
-// Update cart display
-function updateCart() {
-    cartButton.innerText = Cart (${cartCount});
-}
+foods.addEventListener("click", function(){
+   foods.style.color="red";
+   orders.style.color="white";
+   dishs.style.color="white";
+
+})
+
+
+dishs.addEventListener("click", function(){
+   foods.style.color="white";
+   orders.style.color="white";
+   dishs.style.color="red";
+
+})
+
+
+orders.addEventListener("click", function(){
+   foods.style.color="white";
+   orders.style.color="red";
+   dishs.style.color="white";
+
+})
+
+let logBtn = document.getElementById("logBtn");
+
+logBtn.addEventListener("click", function(){
+   document.querySelector(".loginPage").style.display="block";
+})
+
+let loged = document.getElementById("logedBtn");
+
+loged.addEventListener("click", function(){
+
+   let email = document.getElementById("name");
+   let pass = document.getElementById("pass");
+
+   if(email.value == "" || pass.value == ""){
+       alert("Please Enter Email Password")
+   }else{
+       alert("You Loged In");
+       document.querySelector(".loginPage").style.display="none";
+
+   }
+
+})
+
+trackBtn.addEventListener("click", function(){
+   document.querySelector(".tracking").style.display="flex";
+   document.getElementById("food").style.display="none";
+   document.getElementById("order").style.display="none";
+   document.getElementById("dish").style.display="none";
+   document.querySelector(".main").style.display="none";
+   document.getElementById("card2").style.display="none";
+
+
+
+})
